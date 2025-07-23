63496be0901e1370ecf82e7fd80654ce01c00878
thirusakthi-dev <thirusakthi.dev@gmail.com>
2025-07-23 18:29:47 +0530

    FS

---
+++ FindSquareRoot.js
@@ -0,0 +1,12 @@
+const number = 190;
+
+const result = Math.sqrt(number);
+
+console.log(result);
+
+const funNum = (num) => {
+  const result = Math.sqrt(num);
+  console.log(`${num} square root is ${result}`);
+};
+
+funNum(9);
